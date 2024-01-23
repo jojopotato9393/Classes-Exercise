df3477f69dbfacc36190516af712aad14850d29f
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-23 13:44:16 -0600

    test

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Car
+    {
+        public Car() 
+        {
+            
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public void Type()
+        {
+            Console.WriteLine(Make);
+            Console.WriteLine(Model);
+            Console.WriteLine(Year);
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Make = "chevy";
+            car1.Model = "1LE";
+            car1.Year = "2019";
+            Console.WriteLine($"my car is a {car1.Make} {car1.Model} {car1.Year}");
         }
+        
     }
 }
